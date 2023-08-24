b82cefe49142839a4564a75b24414620e22f02ec
technoboss <m.curtisdon@yahoo.co.uk>
2023-08-24 01:20:10 +0100

    Update app files

--- pages/6_📚_Reports.py
+++ pages/6_📚_Reports.py
@@ -7,11 +7,23 @@ import pandas as pd
 import datetime
 from datetime import datetime
 import calendar
+import base64
+import io
+import openpyxl
 
 # Defining some variables to get current Month and year
 month_num = datetime.now().month
 month_name = calendar.month_name[month_num]
 year = datetime.now().year
+
+# Function to convert dataframe to excel 
+def excel_export(df):
+    towrite = io.BytesIO()
+    downloaded_file = df.to_excel(towrite, index=False, header=True)
+    towrite.seek(0)  # reset pointer
+    b64 = base64.b64encode(towrite.read()).decode()  # some strings
+    linko= f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="myfilename.xlsx">Download excel file</a>'
+    return linko
 
 # Setting the web app basic information
 st.set_page_config (page_title="Ouad|Jet",
@@ -52,11 +64,13 @@ with tab1:
         st.subheader(":violet[**Quarterly**] Daily Sales Report 📗")
         st.checkbox("Use container width", value=False, key="use_container_width_A")
         st.dataframe(df_sale, use_container_width=st.session_state.use_container_width_A)
+        st.markdown(excel_export(df_sale), unsafe_allow_html=True)
     with st.expander("Expand to view report 2"):
         # Displaying the result of the sales report query
         st.subheader(f":orange[**{month_name}**] {year} Sales Report By Product 📘")
         st.checkbox("Use container width", value=False, key="use_container_width_B")
         st.dataframe(df_sale2, use_container_width=st.session_state.use_container_width_B)
+        st.markdown(excel_export(df_sale2), unsafe_allow_html=True)
 with tab2:
     st.write("")
 with tab3:
